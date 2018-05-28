90c59c4cdd14ade1e49e7f60ea1fd3e5247ba7d6
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-05-28 07:49:43 +0300

    hopefully make php not freeze now

--- webserver/10lvl.php
+++ webserver/10lvl.php
@@ -3,67 +3,43 @@ $start_t = microtime(TRUE);
 $main_dir = realpath('..');
 $dirsep = DIRECTORY_SEPARATOR;
 $descriptorspec = array(
-    0 => array("pipe", "rb"),
-    1 => array("pipe", "wb"),
+    # 0 => array("pipe", "rb"),
+    # 1 => array("pipe", "wb"),
     2 => array("pipe", "wb")
 );
-$proc_stdout = "";
-$mwlapplier_start_t = microtime(TRUE);
-$proc = proc_open($main_dir.$dirsep.'applier'.$dirsep.'MWLApplier', $descriptorspec, $pipes, $main_dir);
-if(!is_resource($proc)) {
-    echo "Error opening mwlapplier proc";
-    return;
-}
-stream_set_blocking($pipes[1], FALSE);
-$status = proc_get_status($proc);
-while($status['running']) {
-    sleep(0.2); # works nowhere (sleeps for 1sec)
-    # time_nanosleep(0, 200000000); # works only on linux
-    # usleep(200000); # works on windows php5+ and linux
-    $read = fread($pipes[1], 1048576); # try to read 1MB
-    $proc_stdout .= $read;
-    $status = proc_get_status($proc);
-}
-$mwlapplier_end_t = microtime(TRUE);
-if($status["exitcode"] !== 0) {
-    echo "<pre>Error running applier (code $status[exitcode]):\n";
-    echo stream_get_contents($pipes[2]);
-    echo "</pre>";
-    proc_close($proc);
-    return;
-}
+# $proc_stdout = "";
+$applier_stderr = "";
 $newrom_name = tempnam("", "");
-$output_name = tempnam("", "");
-$newrom = fopen($newrom_name, "wb");
-fwrite($newrom, $proc_stdout);
-$applier_stderr = stream_get_contents($pipes[2]);
-fclose($newrom);
-proc_close($proc);
-$flips_start_t = microtime(TRUE);
-$flipsproc = proc_open($main_dir.$dirsep.'flips -b --exact -c ../clean_smw.sfc '.escapeshellarg($newrom_name)." ".escapeshellarg($output_name), $descriptorspec, $flipspipes);
-if(!is_resource($flipsproc)) {
-    echo "Error opening flips proc";
-    return;
-}
-$status = proc_get_status($flipsproc);
-while($status['running']) {
-    sleep(0.2); # works nowhere (sleeps for 1sec)
-    # time_nanosleep(0, 200000000); # works only on linux
-    # usleep(200000); # works on windows php5+ and linux
-    $status = proc_get_status($flipsproc);
-}
-$flips_end_t = microtime(TRUE);
-if($status["exitcode"] !== 0) {
-    echo "<pre>Error running flips (code $status[exitcode]):\n";
-    echo stream_get_contents($flipspipes[2]);
-    echo "\n";
-    echo stream_get_contents($flipspipes[1]);
+$stderr_name = tempnam("", "");
+$old_wd = getcwd();
+chdir($main_dir);
+$mwlapplier_start_t = microtime(TRUE);
+exec($main_dir.$dirsep.'applier'.$dirsep.'MWLApplier >'.escapeshellarg($newrom_name)." 2>".escapeshellarg($stderr_name), NULL, $applier_exitcode);
+$mwlapplier_end_t = microtime(TRUE);
+chdir($old_wd);
+$applier_stderr = file_get_contents($stderr_name);
+unlink($stderr_name);
+if($applier_exitcode !== 0) {
+    echo "<pre>Error running applier (code $applier_exitcode):\n";
+    echo $applier_stderr;
     echo "</pre>";
-    proc_close($flipsproc);
     return;
 }
-proc_close($flipsproc);
+$output_name = tempnam("", "");
+
+$flips_start_t = microtime(TRUE);
+exec($main_dir.$dirsep.'flips -b --exact -c ../clean_smw.sfc '.escapeshellarg($newrom_name)." ".escapeshellarg($output_name), $flips_out, $flips_exitcode);
+$flips_end_t = microtime(TRUE);
+if($flips_exitcode !== 0) {
+    echo "<pre>Error running flips (code $flips_exitcode):\n";
+    echo join("\n", $flips_out);
+    echo "</pre>";
+    return;
+}
+
 $out = file_get_contents($output_name);
+unlink($output_name);
+unlink($newrom_name);
 # header("Content-Type: application/octet-stream");
 # header("Content-Disposition: attachment; filename=smwmaker.bps");
 # echo $out;
