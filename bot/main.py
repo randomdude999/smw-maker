70f8dfcacddb71433e2a5484d259d9d3da3f9a94
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-06-09 12:56:58 +0300

    More work on python bot
    * Added helper function for querying smwc with the session cookie
    * Implemented send_pm
    * Fixed up SQL in handle_user

--- bot/main.py
+++ bot/main.py
@@ -4,12 +4,20 @@ import secrets
 
 import requests
 import MySQLdb
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
+
+bs = lambda text: BeautifulSoup(text, "html.parser")
 
 smwc_endpoint = "https://www.smwcentral.net/"
 pm_template = """Your SMW Maker token is {}. Go to [url]...[/url] to log in now.
 If you need a new token, just PM me again. That will also invalidate the previous token.
 """
+
+def smwc_request(url, data=None):
+    if data is None:
+        return requests.get(smwc_endpoint+url, cookies={"smwc_session":sess_token})
+    else:
+        return requests.post(smwc_endpoint+url, data=data, cookies={"smwc_session":sess_token})
 
 def login(auth_data):
     payload = {
@@ -25,48 +33,65 @@ def login(auth_data):
 
 
 def logout():
-    with requests.get(smwc_endpoint+"?p=login&do=logout", cookies={"smwc_session":sess_token}) as r:
+    with smwc_request("?p=login&do=logout") as r:
         # locate "security token" from that page
         soup = bs(r.text)
         elem = soup.find("input", attrs={"name":"token"})
         if elem:
-            sec_token = elem.value
+            sec_token = elem.attrs['value']
         else:
             return # no token found -> not logged in
     payload = {
         "logout": "Confirm+Logout",
         "token": sec_token
     }
-    requests.post(smwc_endpoint+"?p=login&do=logout", data=payload, cookies={"smwc_session":sess_token})
+    smwc_request("?p=login&do=logout", data=payload)
     # don't even care about the result, we did our best to log out
 
 def send_pm(target, title, text):
-    # TODO
-    pass
+    print(f"PM to {repr(target)} title {repr(title)} text {repr(text)}")
+    with smwc_request("?p=pm&do=compose") as r:
+        soup = bs(r.text)
+        token = soup.find("input", attrs={"name":"token"}).attrs['value']
+    payload = {
+        "do": "compose",
+        "recipient": target,
+        "subject": title,
+        "text": text,
+        "submit": "Submit Message",
+        "token": token
+    }
+    smwc_request("?p=pm", data=payload) # if this actually works then wtf
 
 def handle_user(uname, uid):
     # generate token for user, insert it into the DB and PM them the new token
     cur = conn.cursor()
-    cur.execute("SELECT login_token FROM users")
+    cur.execute("SELECT token FROM users")
     used_tokens = [x[0] for x in cur.fetchall()]
     token = secrets.token_hex(16)
     while token in used_tokens:
         # because repeating code is totally Pythonic™
         token = secrets.token_hex(16)
-    cur.execute("INSERT INTO users (smwc_id, name, token) VALUES (?, ?, ?)", uid, uname, token)
+    # could probably use ON DUPLICATE KEY UPDATE here but the manual says that messes with primary keys
+    cur.execute("SELECT smwc_id FROM users WHERE smwc_id=%s", (uid,))
+    if len(cur.fetchall()) > 0:
+        cur.execute("UPDATE users SET name=%s, token=%s WHERE smwc_id=%s", (uname, token, uid))
+    else:
+        cur.execute("INSERT INTO users (smwc_id, name, token) VALUES (%s, %s, %s)", (uid, uname, token))
+    conn.commit()
     send_pm(uname, "Re: smwmaker verify", pm_template.format(token))
 
 def handle_pm(table_row):
     # handle incoming PM (table_row is the <tr> element of the PM in the PM list)
-    url = smwc_endpoint + table_row.select("td")[1].a.attrs['href'].replace("/","")
-    with requests.get(url, cookies={"smwc_session":sess_token}) as r:
+    url = table_row.select("td")[1].a.attrs['href'].replace("/","")
+    with smwc_request(url) as r:
         # we don't actually need to do anything with this, just do this to mark the PM read
         pass
     uname_elem = table_row.select("td")[2].a
     if 'title' in uname_elem.attrs:
-        uname = uname_elem.attrs['title']
+        uname = str(uname_elem.attrs['title'])
     else:
-        uname = uname_elem.string
+        uname = str(uname_elem.string)
     u_id = int(uname_elem.attrs['href'].split('id=')[1])
     handle_user(uname, u_id)
 
@@ -74,7 +99,7 @@ def check_smwc():
     global sess_token
     # using a while loop here is hacky but it's the best alternative to a goto (which doesn't even exist in python :( )
     while True:
-        with requests.get(smwc_endpoint+"?p=pm", cookies={"smwc_session":sess_token}) as r:
+        with smwc_request("?p=pm") as r:
             soup = bs(r.text)
             if soup.find(id="deleteform"):
                 # we are logged in
