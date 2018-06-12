0448a7e072ce9069686ae4b40be60cc38d7632b9
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-06-12 14:07:33 +0300

    mostly finish uploading

--- webserver/upload.php
+++ webserver/upload.php
@@ -13,11 +13,12 @@ if(get($_SESSION["logged_in"], false) === false) {
 <body>
 	<p>Upload level</p>
 	<form action="upload_.php" enctype="multipart/form-data" method="POST">
-		<label for="lvlname">Level name:</label> <input type="text" name="lvlname" placeholder="Level name" required><br>
+		<input type="hidden" name="MAX_FILE_SIZE" value="2097152"> <!-- mirroring PHP's default upload_max_filesize -->
+		<label for="lvlname">Level name:</label> <input type="text" name="lvlname" placeholder="Level name" required maxlength="255"><br>
 		<label for="difficulty">Difficulty:</label> <input type="range" name="difficulty" min=0 max=3 step=1 id="dif"><span id="diftxt"></span><br>
 		<label for="mainfile">Main level:</label> <input type="file" name="mainfile" accept=".mwl" required><br>
-		<label for="hassub">Has sublevel:</label> <input type="checkbox" name="hassub"><br>
-		<label for="subfile">Sublevel:</label> <input type="file" name="subfile" accept=".mwl"><br>
+		<label for="hassub">Has sublevel:</label> <input type="checkbox" name="hassub" id="hassub"><br>
+		<label for="subfile">Sublevel:</label> <input type="file" name="subfile" id="subfile" accept=".mwl"><br>
 		<input type="submit" value="Upload">
 	</form>
 	<script>
@@ -27,10 +28,14 @@ if(get($_SESSION["logged_in"], false) === false) {
 			"Hard",
 			"Kaizo"
 		];
-		document.getElementById("dif").oninput = function(e) {
-			document.getElementById("diftxt").innerHTML = difficulties[e.target.value];
+		document.getElementById("dif").oninput = function() {
+			document.getElementById("diftxt").innerHTML = difficulties[document.getElementById("dif").value];
 		};
-		document.getElementById("diftxt").innerHTML = difficulties[document.getElementById("dif").value];
+		document.getElementById("dif").oninput();
+		document.getElementById("hassub").onchange = function() {
+			document.getElementById("subfile").disabled = !document.getElementById("hassub").checked;
+		}
+		document.getElementById("hassub").onchange();
 	</script>
 </body>
 </html>
--- webserver/upload_.php
+++ webserver/upload_.php
@@ -3,12 +3,78 @@ include 'common_includes.php';
 $mysqli = connect_db();
 session_start();
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-	if(get($_SESSION["logged_in"], false) === false) {
-		echo "Not logged in.";
+function uploadCodeToMsg($code) {
+	switch ($code) {
+		case UPLOAD_ERR_OK:
+			return "Upload was successful";
+		case UPLOAD_ERR_INI_SIZE:
+			return "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+		case UPLOAD_ERR_FORM_SIZE:
+			return "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+		case UPLOAD_ERR_PARTIAL:
+			return "The uploaded file was only partially uploaded";
+		case UPLOAD_ERR_NO_FILE:
+			return "No file was uploaded";
+		case UPLOAD_ERR_NO_TMP_DIR:
+			return "Missing a temporary folder";
+		case UPLOAD_ERR_CANT_WRITE:
+			return "Failed to write file to disk";
+		case UPLOAD_ERR_EXTENSION:
+			return "File upload stopped by extension";
+		default:
+			return "Unknown upload error"; 
+	}
+}
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+	if(get($_SESSION["logged_in"], false) === false)
+		die("Not logged in.");
+	if(empty($_POST["lvlname"]) || empty($_POST["difficulty"]))
+		die("Invalid request.");
+	$difficulty = $_POST["difficulty"];
+	$level_name = $_POST["lvlname"];
+	$hassub = isset($_POST["hassub"]);
+	if(empty($_FILES["mainfile"]))
+		die("No main file provided");
+	if($hassub && empty($_FILES["subfile"]))
+		die("No sublevel file provided");
+	if(strlen($level_name) > 255)
+		die("lvlname too long");
+	if(!ctype_digit($difficulty))
+		die("Invalid difficulty");
+	if(intval($difficulty) > 3) // it can't be <0 because that would require non-digit chars
+		die("Invalid difficulty");
+	$difficulty = intval($difficulty);
+	if($_FILES["mainfile"]["error"] !== 0)
+		die(uploadCodeToMsg($_FILES["mainfile"]["error"]));
+	if(!empty($_POST["hassub"]) && $_FILES["subfile"]["error"] !== 0)
+		die(uploadCodeToMsg($_FILES["subfile"]["error"]));
+	exec(realpath("..")."/checkmwl.py ".escapeshellarg($_FILES["mainfile"]["tmp_name"])." 2>&1", $output, $retcode);
+	if($retcode !== 0) {
+		echo "Error running MWL checker. This doesn't look like a valid MWL file. <pre>";
+		echo join("\n", $output);
+		echo "</pre>";
 		return;
 	}
-	# TODO
+	if($hassub) {
+		exec(realpath("..")."/checkmwl.py ".escapeshellarg($_FILES["subfile"]["tmp_name"])." 2>&1", $output, $retcode);
+		if($retcode !== 0) {
+			echo "Error running MWL checker. This doesn't look like a valid MWL file. <pre>";
+			echo join("\n", $output);
+			echo "</pre>";
+			return;
+		}
+	}
+	$stmt = $mysqli->prepare("INSERT INTO levels (name, author, difficulty, verified) VALUES (?, ?, ?, 0)");
+	if($stmt === FALSE)
+		die("MySQL error: ".$mysqli->error);
+	$stmt->bind_param("sii", $level_name, $_SESSION["user_id"], $difficulty);
+	if(!$stmt->execute())
+		die("MySQL error: ".$mysqli->error);
+	$id = $mysqli->insert_id;
+	rename($_FILES["mainfile"]["tmp_name"], "../levels/${id}_main.mwl");
+	if($hassub) rename($_FILES["subfile"]["tmp_name"], "../levels/${id}_sub.mwl");
+	redirect("index.php");
 } else {
 	redirect("upload.php");
 }
