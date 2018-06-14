8d072c4d9fa09cb7f3d5b79bd39eee642ba0535e
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-06-14 15:33:48 +0300

    basic mwl checking, should prevent possible segfaults in inserter

--- checkmwl.py
+++ checkmwl.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python3
+import sys
+import struct
 
-# TODO: write this
+sections = [
+	"level",
+	"layer1",
+	"layer2",
+	"sprite",
+	"palette",
+	"entrance",
+	"exanim",
+	"bypass"
+]
+
+with open(sys.argv[1], 'rb') as f:
+	data = f.read()
+
+if len(data) < 0x40:
+	print("Invalid MWL header")
+	sys.exit(1)
+
+if data[0:2] != b"LM":
+	print("Invalid MWL header")
+	sys.exit(1)
+
+(data_ptr_start, data_ptr_len) = struct.unpack("<ii", data[4:12])
+if data_ptr_start+data_ptr_len > len(data):
+	print("Invalid MWL header")
+	sys.exit(1)
+
+for i in range(0, data_ptr_len, 8):
+	sec = sections[i//8]
+	(secptr, seclen) = struct.unpack("<ii", data[data_ptr_start + i : data_ptr_start+i+8])
+	if secptr+seclen > len(data):
+		print(f"Invalid MWL pointer table")
+		sys.exit(1)
+
+# at this point we shouldn't get segfaults while inserting levels, but they could still crash the ROM
